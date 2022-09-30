958f109a44262e265925be03b051435b8b90b53f
QkeleQ10 <quinten@althues.nl>
2022-09-30 21:41:05 +0200

    Toelichtingen

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const musicPlr = document.querySelector('#music-player')
 const musicSrc = document.querySelector('#music-source')
 
+document.querySelectorAll('.tip').forEach(e => {
+    e.addEventListener('click', event => {
+        event.stopPropagation()
+        document.getElementById('popup-body').innerText = event.target.getAttribute('data-tip')
+        document.getElementById('popup').classList.add('open')
+        event.target.classList.add('open')
+    })
+})
+
 document.querySelectorAll('.music-card').forEach(e => {
-    e.addEventListener('click', function () {
+    e.addEventListener('click', () => {
         const src = `music/${e.id}.mp3`
         document.querySelectorAll('.playing').forEach(e1 => e1.classList.remove('playing'))
         if (musicSrc.getAttribute('src') != src) {
